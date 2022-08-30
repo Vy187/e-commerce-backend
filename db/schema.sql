3ab813b67db2c947be9764cf5c9b48fd3e04b24b
Vy Nguyen <vy1872@gmail.com>
2022-08-30 13:36:36 -0700

    just setup refences for models

--- db/schema.sql
+++ db/schema.sql
@@ -1,31 +1,31 @@
 DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
-USE ecommerce_db;
+-- USE ecommerce_db;
 
-CREATE TABLE category (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(30) NOT NULL
-);
+-- CREATE TABLE category (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     category_name VARCHAR(30) NOT NULL
+-- );
 
-CREATE TABLE product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_name VARCHAR(30) NOT NULL,
-    price DECIMAL NOT NULL,
-    stock INT NOT NULL DEFAULT 10,
-    category_id INT,
-    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
-);
+-- CREATE TABLE product (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     product_name VARCHAR(30) NOT NULL,
+--     price DECIMAL NOT NULL,
+--     stock INT NOT NULL DEFAULT 10,
+--     category_id INT,
+--     FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE SET NULL
+-- );
 
-CREATE TABLE tag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(30),
-);
+-- CREATE TABLE tag (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     tag_name VARCHAR(30),
+-- );
 
-CREATE TABLE product_tag (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    product_id INT,
-    tag_id INT,
-    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL,
-    FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE SET NULL
-);
+-- CREATE TABLE product_tag (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     product_id INT,
+--     tag_id INT,
+--     FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE SET NULL,
+--     FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE SET NULL
+-- );
--- models/index.js
+++ models/index.js
@@ -3,9 +3,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+Product.belongsTo(Category, {
+  foreignKey: `category_id`
+});
+
+Category.hasMany(Product, {
+  foreignKey: `category_id`
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: `tag_id`
+  }
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: `product_id`
+  }
+});
+
+module.exports = { Product, Category, Tag, ProductTag };

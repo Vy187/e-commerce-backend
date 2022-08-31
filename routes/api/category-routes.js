03f2fc8d7e201ae19c78921aac156b0099b596f4
Vy Nguyen <vy1872@gmail.com>
2022-08-31 12:36:18 -0700

    forgot to include the model products for findAll for route to get all the categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,7 +3,7 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({ include: [{ model: Product }]});
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);

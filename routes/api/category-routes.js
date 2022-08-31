0e848399401a7960d04bf94c904c8dbe517d8a32
Vy Nguyen <vy1872@gmail.com>
2022-08-31 02:57:11 -0700

    made the put route for model catergory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,21 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {

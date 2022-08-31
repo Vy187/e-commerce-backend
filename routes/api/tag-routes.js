64ad7499a571e4ed638ee203357f65b77969453a
Vy Nguyen <vy1872@gmail.com>
2022-08-31 13:12:23 -0700

    got the route to find a specific tag data to be sending the right data now

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,8 +10,16 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const tagData = await Tag.findByPk(req.params.id, { include: [{ model: Product }] });
+        if(!tagData) {
+            return res.status(404).json({ message: `No tag found with this id` });
+        }
+        res.status(200).json(tagData);
+    } catch (err) {
+        res.staus(500).json(err);
+    }
 });
 
 router.post('/', async (req, res) => {

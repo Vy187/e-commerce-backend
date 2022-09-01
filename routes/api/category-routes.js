ac23e8e73f7e8050d24907b0f2d5a69288e42245
Vy Nguyen <vy1872@gmail.com>
2022-08-31 18:36:12 -0700

    changed the foreign keys to allow it to be null so I can delete or update and deleting one catergory works now

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,8 +35,16 @@ router.put('/:id', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({ where: { id: req.params.id } });
+    if (!categoryData) {
+      return res.status(404).json({ message: `No category found with this id` });
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
